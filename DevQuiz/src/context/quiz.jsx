8392c45a04dab13afc5ba5052a7c3992a8ac0fb5
AdrianoJH <adrianorsouza.h6@gmail.com>
2022-11-08 15:48:37 -0300

    Sem responsividade ainda

--- DevQuiz/src/context/quiz.jsx
+++ DevQuiz/src/context/quiz.jsx
@@ -7,6 +7,8 @@ const initialState = {
     gameStage: STAGES[0],
     questions,
     currentQuestions: 0,
+    score: 0,
+    answerSelected: false,
      
 };
 
@@ -42,7 +44,27 @@ const quizReducer = (state, action) => {
                 ...state,
                 currentQuestions: nextQuestion,
                 gameStage: endGame ? STAGES[2] : state.gameStage,
+                answerSelected: false,
             };
+
+            case "NEW_GAME":
+                return initialState;
+
+            case "CHECK_ANSWER":
+                if (state.answerSelected) return state;
+
+                const answer = action.payload.answer;
+                const option = action.payload.option;
+                let correctAnswer = 0;
+
+                if (answer === option) correctAnswer = 1;
+
+                return {
+                    ...state,
+                    score: state.score + correctAnswer,
+                    answerSelected: option,
+                };
+
 
         default:
             return state;
